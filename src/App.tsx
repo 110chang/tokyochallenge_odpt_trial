ee596ea3479c59716d75d45ecbbea34f740d5a58
Yuji Ito <u@110chang.com>
2021-06-29 15:33:46 +0900

    APIから取得してみた

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`https://api-tokyochallenge.odpt.org/api/v4/odpt:Train?acl:consumerKey=${process.env.REACT_APP_ACL_CONSUMERKEY}`)
+      console.log(res)
+      const json = await res.json();
+      console.log(json)
+    })()
+  })
   return (
     <div className="App">
       <header className="App-header">
